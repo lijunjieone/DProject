40c6744ba3bb2589530d065b4fbafefb4f7dffa5
lijunjie <lijunjie@windimg.com>
2020-11-05 18:02:50 +0800

    add some code for simple activity

--- app/src/main/java/com/a/dproject/MainActivity.kt
+++ app/src/main/java/com/a/dproject/MainActivity.kt
@@ -2,10 +2,18 @@ package com.a.dproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.a.dproject.mvvm.fragment.ListFragment
+import com.a.dproject.mvvm.fragment.SimpleFragment
+import com.sankuai.waimai.router.annotation.RouterUri
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_simple)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, SimpleFragment.newInstance())
+                    .commitNow()
+        }
     }
 }
