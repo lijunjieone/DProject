f13c6767b4a0b9de11bb2de32e3f5ce8ceff97e0
lijunjie <lijunjie@windimg.com>
2020-11-18 12:23:37 +0800

    申请存储权限

--- app/src/main/java/com/a/dproject/mvvm/activity/SimpleActivity.kt
+++ app/src/main/java/com/a/dproject/mvvm/activity/SimpleActivity.kt
@@ -1,10 +1,13 @@
 package com.a.dproject.mvvm.activity
 
+import android.Manifest
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.a.dproject.R
 import com.a.dproject.mvvm.fragment.ListFragment
 import com.hunter.library.debug.HunterDebugClass
+import com.permissionx.guolindev.PermissionX
 
 @HunterDebugClass
 class SimpleActivity : AppCompatActivity() {
@@ -21,6 +24,20 @@ class SimpleActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+
+        PermissionX.init(this)
+            .permissions(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            .request { allGranted, grantedList, deniedList ->
+                if (allGranted) {
+                    Toast.makeText(this, "All permissions are granted", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "These permissions are denied: $deniedList",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
     }
 
     override fun onStop() {
