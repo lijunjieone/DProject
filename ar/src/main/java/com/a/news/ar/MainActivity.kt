6f8734428a5ea319182963e0dad5ca039140237a
lijunjie <lijunjie@windimg.com>
2021-01-08 13:17:25 +0800

    增加Activity性能日志

--- ar/src/main/java/com/a/news/ar/MainActivity.kt
+++ ar/src/main/java/com/a/news/ar/MainActivity.kt
@@ -2,10 +2,24 @@ package com.a.news.ar
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.a.news.ar.profile.monitor.time.TimeMonitorConfig
+import com.a.news.ar.profile.monitor.time.TimeMonitorManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        TimeMonitorManager.getInstance().getTimeMonitor(TimeMonitorConfig.TIME_MONITOR_ID_APPLICATION_START).recodingTimeTag("AppStartActivity_create")
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        TimeMonitorManager.getInstance().getTimeMonitor(TimeMonitorConfig.TIME_MONITOR_ID_APPLICATION_START).recodingTimeTag("AppStartActivity_createOver")
+
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        TimeMonitorManager.getInstance().getTimeMonitor(TimeMonitorConfig.TIME_MONITOR_ID_APPLICATION_START).end("AppStartActivity_start", false)
+    }
+
 }
