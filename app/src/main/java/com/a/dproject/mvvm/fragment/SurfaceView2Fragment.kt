3a001998836375abbb9f6c0a6deaf5d000b25b24
lijunjie <lijunjie@windimg.com>
2021-01-13 11:47:21 +0800

    使用ArSenceView来初始化

--- app/src/main/java/com/a/dproject/mvvm/fragment/SurfaceView2Fragment.kt
+++ app/src/main/java/com/a/dproject/mvvm/fragment/SurfaceView2Fragment.kt
@@ -3,16 +3,27 @@ package com.a.dproject.mvvm.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.a.dproject.R
+import com.a.dproject.ar.utils.DemoUtils
 import com.a.dproject.databinding.FragmentSurfaceView2Binding
 import com.a.dproject.mvvm.viewmodel.SurfaceView2ViewModel
 import com.a.dproject.toast
 import com.a.processor.ListFragmentAnnotation
+import com.google.ar.core.Config.LightEstimationMode
+import com.google.ar.core.Session
+import com.google.ar.core.exceptions.CameraNotAvailableException
+import com.google.ar.core.exceptions.UnavailableException
+import com.google.ar.sceneform.ArSceneView
+import com.google.ar.sceneform.FrameTime
+import com.google.ar.sceneform.HitTestResult
+import com.google.ar.sceneform.Scene
+import com.google.ar.sceneform.Scene.OnUpdateListener
 
 
 @ListFragmentAnnotation("arCore学习", parentName = "ArCore")
@@ -21,6 +32,7 @@ class SurfaceView2Fragment : ArtBaseFragment(), View.OnClickListener {
     protected lateinit var binding: FragmentSurfaceView2Binding
     lateinit var viewModel: SurfaceView2ViewModel
 
+    lateinit var arSceneView: ArSceneView
     var id: Long = 0L
 
 
@@ -33,7 +45,11 @@ class SurfaceView2Fragment : ArtBaseFragment(), View.OnClickListener {
     }
 
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         binding = DataBindingUtil.inflate(inflater, getContentId(), container, false)
         initViewModel()
@@ -62,11 +78,87 @@ class SurfaceView2Fragment : ArtBaseFragment(), View.OnClickListener {
     private fun initObserver() {
     }
 
+    override fun onResume() {
+        super.onResume()
+        if (arSceneView == null) {
+            return
+        }
+
+        if (arSceneView.getSession() == null) {
+            // If the session wasn't created yet, don't resume rendering.
+            // This can happen if ARCore needs to be updated or permissions are not granted yet.
+            try {
+                val lightEstimationMode = LightEstimationMode.ENVIRONMENTAL_HDR
+                val session: Session =
+                    DemoUtils.createArSessionWithInstallRequest(
+                        requireActivity(),
+                        lightEstimationMode
+                    )
+                arSceneView.setupSession(session)
+            } catch (e: UnavailableException) {
+                DemoUtils.handleSessionException(requireActivity(), e)
+            }
+        }
+
+        try {
+            arSceneView.resume()
+        } catch (ex: CameraNotAvailableException) {
+            DemoUtils.displayError(requireContext(), "Unable to get camera", ex)
+            return
+        }
+
+//        if (arSceneView.getSession() != null) {
+//            showLoadingMessage()
+//        }
+    }
 
     private fun initView() {
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
         binding.onClickListener = this
+        arSceneView = binding.arSceneView
+
+
+        // Set a touch listener on the Scene to listen for taps.
+        binding.arSceneView
+            .getScene()
+            .setOnTouchListener(
+                Scene.OnTouchListener { hitTestResult: HitTestResult?, event: MotionEvent? ->
+                    // If the solar system hasn't been placed yet, detect a tap and then check to see if
+                    // the tap occurred on an ARCore plane to place the solar system.
+//                    if (!hasPlacedSolarSystem) {
+//                        return@setOnTouchListener gestureDetector.onTouchEvent(event)
+//                    }
+                    event.toString().toast()
+                    false
+                })
+
+        // Set an update listener on the Scene that will hide the loading message once a Plane is
+        // detected.
+
+        // Set an update listener on the Scene that will hide the loading message once a Plane is
+        // detected.
+        binding.arSceneView
+            .getScene()
+            .addOnUpdateListener(
+                OnUpdateListener { frameTime: FrameTime? ->
+                    frameTime.toString().toast()
+//                    if (loadingMessageSnackbar == null) {
+//                        return@addOnUpdateListener
+//                    }
+//                    val frame: Frame = arSceneView.getArFrame() ?: return@addOnUpdateListener
+//                    if (frame.camera.trackingState != TrackingState.TRACKING) {
+//                        return@addOnUpdateListener
+//                    }
+//                    for (plane in frame.getUpdatedTrackables(
+//                        Plane::class.java
+//                    )) {
+//                        if (plane.trackingState == TrackingState.TRACKING) {
+//                            hideLoadingMessage()
+//                        }
+//                    }
+                })
+
 
     }
 
@@ -77,9 +169,6 @@ class SurfaceView2Fragment : ArtBaseFragment(), View.OnClickListener {
     override fun onClick(p0: View?) {
         p0?.let {
             when (it) {
-                binding.message -> {
-                    "message".toast()
-                }
                 binding.tvEvent -> {
                     "event".toast()
                 }
