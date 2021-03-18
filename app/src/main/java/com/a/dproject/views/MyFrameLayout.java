18c027bb4c8cc932f56fdd3e920d67bd59559af3
lijunjie <lijunjie@windimg.com>
2021-03-18 20:27:53 +0800

    传递Context进入页面

--- app/src/main/java/com/a/dproject/views/MyFrameLayout.java
+++ app/src/main/java/com/a/dproject/views/MyFrameLayout.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
@@ -29,5 +30,33 @@ public class MyFrameLayout extends FrameLayout {
         Paint paint = new Paint();
         paint.setColor(Color.RED);
         canvas.drawCircle(50,50,100,paint);
+//        testFoo();
+        testOperater();
+    }
+
+    public void testOperater() {
+        StringBuffer a= new StringBuffer("A");
+        StringBuffer b= new StringBuffer("B");
+        operater(a,b);
+        Log.e("Test",a+","+b);
+
+    }
+
+    public void operater(StringBuffer x,StringBuffer y) {
+        x.append(y);
+        y=x;
+    }
+
+    public boolean foo(String chr) {
+        Log.e("Test",chr);
+        return true;
+    }
+
+    public void testFoo() {
+        int i=0;
+        for(foo("A");foo("B")&&(i<2);foo("C")) {
+            i++;
+            foo("D");
+        }
     }
 }
