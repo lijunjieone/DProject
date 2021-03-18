afc0d79ca82565caae508dc47d23ca8556208418
lijunjie <lijunjie@windimg.com>
2021-03-18 21:10:17 +0800

    传递Context进入页面

--- app/src/main/java/com/a/dproject/views/MyFrameLayout.java
+++ app/src/main/java/com/a/dproject/views/MyFrameLayout.java
@@ -11,6 +11,12 @@ import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class MyFrameLayout extends FrameLayout {
     public MyFrameLayout(@NonNull Context context) {
         super(context);
@@ -31,7 +37,10 @@ public class MyFrameLayout extends FrameLayout {
         paint.setColor(Color.RED);
         canvas.drawCircle(50,50,100,paint);
 //        testFoo();
-        testOperater();
+//        testOperater();
+//        Log.e("Test",removeContinuousDuplicate("aabbcceeffssgag"));
+//        testPrint();
+        testStack();
     }
 
     public void testOperater() {
@@ -40,6 +49,97 @@ public class MyFrameLayout extends FrameLayout {
         operater(a,b);
         Log.e("Test",a+","+b);
 
+    }
+
+
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        while(!stack1.empty()){
+            stack2.push(stack1.pop());
+        }
+        stack1.push(node);
+        while(!stack2.empty()){
+            stack1.push(stack2.pop());
+        }
+    }
+
+    public int pop() {
+        return stack1.pop();
+    }
+
+
+    public void testStack() {
+        for(int i=0;i<10;i++) {
+            push(i);
+        }
+
+        for(int i=0;i<stack1.size();i++) {
+            Log.e("Test","num:"+pop());
+        }
+
+    }
+
+
+    Lock lock = new ReentrantLock();
+
+    class ThreadA1 extends  Thread {
+        String label="";
+        ThreadA1(String label) {
+            this.label = label;
+        }
+        @Override
+        public void run() {
+            for(int i = 0;i<10;i++) {
+                lock.lock();
+                Log.e("Test","Label:"+label);
+                lock.unlock();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+
+    public void testPrint() {
+        ThreadA1 a = new ThreadA1("1");
+        ThreadA1 b = new ThreadA1("2");
+
+        a.start();
+        b.start();
+
+
+    }
+    String removeContinuousDuplicate(String input) {
+        StringBuffer a = new StringBuffer();
+        String lastChar = "";
+        for(int i = 0;i<input.length();i++) {
+             String currentChar = input.substring(i,i+1);
+             if(!lastChar.equals(currentChar)) {
+                 a.append(currentChar);
+                 lastChar = currentChar;
+             }
+        }
+
+        return a.toString();
+    }
+
+    public static void deleteStringGt(List<String> input, int n) {
+        List result = new ArrayList();
+        for(int i=0;i<input.size();i++) {
+            String t = input.get(i);
+            if(t.length()<n) {
+                result.add(t);
+            }
+
+        }
+        input.clear();
+        input.addAll(result);
     }
 
     public void operater(StringBuffer x,StringBuffer y) {
